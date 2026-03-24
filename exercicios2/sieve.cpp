62d16e61a4400b03caff4c1cc5be3205d9d6f4fb
herich0 <herichop013@gmail.com>
2026-03-24 10:38:48 -0300

    desafio extra

--- exercicios2/sieve.cpp
+++ exercicios2/sieve.cpp
@@ -2,19 +2,37 @@
 #include <stdbool.h>
 #include <string.h>
 #include <omp.h>
+#include <math.h>
 
 int main() {
     int n = 1000;
     bool primo[1001];
-    
     memset(primo, true, sizeof(primo));
 
-    for (int p = 2; p * p <= n; p++) {
+    int limite = (int)sqrt(n);
+
+    for (int p = 2; p * p <= limite; p++) {
         if (primo[p]) {
-            #pragma omp parallel for schedule(dynamic)
-            for (int i = p * p; i <= n; i += p) {
+            for (int i = p * p; i <= limite; i += p) {
                 primo[i] = false;
             }
+        }
+    }
+
+    int primos_base[limite + 1];
+    int cont_base = 0;
+    for (int p = 2; p <= limite; p++) {
+        if (primo[p]) {
+            primos_base[cont_base] = p;
+            cont_base++;
+        }
+    }
+
+    #pragma omp parallel for schedule(dynamic)
+    for (int k = 0; k < cont_base; k++) {
+        int p = primos_base[k];
+        for (int i = p * p; i <= n; i += p) {
+            primo[i] = false;
         }
     }
 
